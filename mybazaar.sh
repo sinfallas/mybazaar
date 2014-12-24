b1a5f587501ef3ea3780ca10a63a9bec87c16cb1
sinfallas <sinfallas@gmail.com>
2014-12-23 20:52:57 -0430

    * cambios menores

--- mybazaar.sh
+++ mybazaar.sh
@@ -10,19 +10,43 @@ case "$1" in
 	init)
 		bzr init
 		bzr add
-		bzr commit 
+		bzr commit -m "* primer commit"
 		bzr push $bzrvar
 		;;
 
 	change)
+		bzr check
 		bzr log > CHANGES
 		bzr add
 		bzr commit -m "* changelog"
 		bzr push $bzrvar
 		;;
 
+	change-all)
+		for i in $(find . -maxdepth 1 -type d | cut -c 3-50); do
+			cd $i
+				bzrvar="lp:$(cat .bzr/branch/branch.conf | grep push_location | cut -c 48-99)"
+				echo $(pwd)
+				bzr check
+				bzr log > CHANGES
+				bzr add
+				bzr commit -m "* changelog"
+				bzr push $bzrvar2
+			cd ..
+		done
+		;;
+
+	refresh)
+		for i in $(find . -maxdepth 1 -type d | cut -c 3-50); do
+    			cd $i
+			echo $(pwd)
+			bzr pull
+			cd ..
+		done
+		;;
+
 	*)
-		echo "USO: $0 {up|init|change}"
+		echo "USO: $0 {up|init|change|refresh}"
 		;;
 esac
 exit 0
